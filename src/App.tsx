3612a5d72c92dca18261e4833577486398ab1a7c
GAT <mozzarellaexpertise@gmail.com>
2025-10-03 15:45:41 +0630

    Remove Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login";
 
 export default function App() {
   return (
     <Router>
       <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
         <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-        <Link to="/login">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
