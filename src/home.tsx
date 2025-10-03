aa4d05f583ba82b5a05e6c693b0f95b864c09ef4
GAT <mozzarellaexpertise@gmail.com>
2025-10-03 15:22:37 +0630

    Fix casing for home.tsx

--- src/home.tsx
+++ src/home.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
-      <h1>Hello World 👋</h1>
-      <p>Welcome to my classic React SPA!</p>
+      <h1>Home Page</h1>
+      <p>Welcome to your React SPA!</p>
     </div>
   );
 }
